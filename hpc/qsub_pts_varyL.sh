06ef0d6cd3fdd02cbf7bee1103e476e417ba2e29
adamtclark <adam.tclark@gmail.com>
2019-04-05 11:12:48 +0200

    Added new files for variable L

---
+++ hpc/qsub_pts_varyL.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#------------------------------------------
+#$ -N pts
+#$ -o iman.out$JOB_ID
+#$ -j y
+#$ -S /bin/bash
+#$ -l h_rt=4:00:00 
+#$ -l h_vmem=6G
+#$ -pe smp 1
+#$ -t 1-600
+
+date
+module load R
+cd /home/clarka/pts_r_package/hpc/
+./run_ptstab_proc_varyL.R $SGE_TASK_ID
+date
---
+++ hpc/run_ptstab_proc_varyL.R
@@ -0,0 +1,90 @@
+#!/usr/bin/env Rscript
+
+if(FALSE) {
+  error
+  rm(list=ls())
+  setwd("~/Dropbox/Projects/041_Powerscaling_stability/src/pts_r_package/hpc/")
+}
+
+## Load functions:
+commArgin<-commandArgs(1)
+if(length(commArgin)==0) {
+  commArgin<-round(runif(1)*1e6)
+  commArg_ps<-1
+} else {
+  commArg_ps<-as.numeric(commArgin)
+}
+
+#process error to use
+nsteps<-6; nitertot<-100
+lsq<-rep(c(10, 20, 30, 50, 75, 100), each=nitertot)
+print(commArg_ps)
+
+require(BayesianTools)
+require(rEDM)
+
+source("../pttstability/R/bayesfun.R")
+source("../pttstability/R/fake_data.R")
+source("../pttstability/R/logit_funs.R")
+source("../pttstability/R/particlefilter.R")
+
+
+## Simulate data
+pars<-list(obs=c(log(1e-2), log(0.1)),
+           proc=c(-2, log(1.5)),
+           pcol=c(logit(0.2), log(1e-2)),
+           det=c(log(3),log(1)))
+pars_sim<-pars
+
+datout<-makedynamics(n = lsq[commArg_ps], obs = pars_sim$obs, proc = pars_sim$proc, r = pars_sim$det[1],
+                     K = pars_sim$det[2], pcol = pars_sim$pcol)
+y<-datout$obs
+
+## Run filter
+N = 1e3
+
+## Run optimizer
+param<-unlist(pars)[1:6]
+
+#create priors
+density_fun_USE<-function(param) density_fun0(param = param, pars = pars)
+sampler_fun_USE<-function(x) sampler_fun0(n = 1, pars = pars)
+prior <- createPrior(density = density_fun_USE, sampler = sampler_fun_USE,
+                     lower = rep(-10,6), upper = rep(10,6))
+
+#number of MCMC iterations - increase for more accurate results
+#note - runtime will be long for EDM example
+niter<-5001
+nburn<-1002
+
+#with detfun0
+likelihood_detfun0<-function(x) likelihood0(param=x, y=y, parseparam = parseparam0)
+bayesianSetup_detfun0 <- createBayesianSetup(likelihood = likelihood_detfun0, prior = prior)
+out_detfun0 <- runMCMC(bayesianSetup = bayesianSetup_detfun0,
+                       settings = list(iterations=niter, burnin=nburn))
+
+#with EDM
+likelihood_EDM<-function(x) likelihood0(param = x, y=y, parseparam = parseparam0,
+                                        detfun = EDMfun0, edmdat = list(E=2))
+bayesianSetup_EDM <- createBayesianSetup(likelihood = likelihood_EDM, prior = prior)
+out_EDM <- runMCMC(bayesianSetup = bayesianSetup_EDM,
+                   settings = list(iterations=niter, burnin=nburn))
+
+#run PF's
+smp_detfun0_untr<-(getSample(out_detfun0))
+smp_EDM_untr<-(getSample(out_EDM))
+
+pfdet<-particleFilterLL(y=datout$obs, pars=parseparam0(colMeans(smp_detfun0_untr)), detfun = detfun0, dotraceback = TRUE)
+pfedm<-particleFilterLL(y=datout$obs, pars=parseparam0(colMeans(smp_EDM_untr)), detfun = EDMfun0, edmdat = list(E=2), dotraceback = TRUE)
+pftrue<-particleFilterLL(y=datout$obs, pars=pars, detfun = detfun0, dotraceback = TRUE)
+
+truecol<-sum(datout$true[-1]>0 & datout$true[-length(datout$true)]==0)/sum(datout$true[-length(datout$true)]==0)
+truemor<-sum(datout$true[-1]==0 & datout$true[-length(datout$true)]>0)/sum(datout$true[-length(datout$true)]>0)
+
+demdat<-list(pfdet=pfdet, pfedm=pfedm, pftrue=pftrue, truecol=truecol, truemor=truemor)
+
+#save outputs
+save(list = c("out_detfun0", "out_EDM", "pars_sim", "datout", "demdat"), file = paste("datout/mcmcout_", commArgin, "_varyL.rda", sep=""))
+
+
+
