ceb8faeaf2aac621c7bbca7e452c37feb5699bf1
adamtclark <adam.tclark@gmail.com>
2019-09-10 16:49:17 +0200

    added qsub

---
+++ hpc/qsub_run_examples.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#------------------------------------------
+#$ -N pts_all
+#$ -o iman.out$JOB_ID
+#$ -j y
+#$ -S /bin/bash
+#$ -l h_rt=6:00:00 
+#$ -l h_vmem=6G
+#$ -pe smp 1
+
+date
+module load R
+cd /home/clarka/pts_r_package/pttstability/
+./pttstability_man_example.R
+date
