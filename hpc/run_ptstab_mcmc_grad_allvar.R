8829c96b84cb72bbe15699560d5fad43155463b6
atclark <adam.tclark@gmail.com>
2019-11-16 17:18:34 +0100

    Updated to run with new code

--- hpc/run_ptstab_mcmc_grad_allvar.R
+++ hpc/run_ptstab_mcmc_grad_allvar.R
@@ -28,24 +28,29 @@ source("../pttstability/R/particlefilter.R")
 
 ## Simulate data
 #sample from priors
-pars0<-list(obs=(-2.5),
-                proc=(-2.5),
-                pcol=c((-1.5), NA),
-                det=c(log(3),log(1)))
-pars0$pcol[2]<-pars0$proc
-
-search_rng<-2
-prior_sd<-2
+pars0<-pars_true<-list(obs=log(0.2),
+                       proc=c(log(0.1)),
+                       pcol=c(logit(0.2), log(0.1)),
+                       det=c(log(2),log(1)))
 
 #create priors
-density_fun_USE<-function(param) density_fun0(param = param, pars = pars0, priorsd = rep(prior_sd, 3))
-sampler_fun_USE<-function(x) sampler_fun0(n = 1, pars = pars0, priorsd = rep(prior_sd, 3),
-                                          minv = c(pars0$obs, pars0$proc, pars0$pcol[1])-search_rng,
-                                          maxv = c(pars0$obs, pars0$proc, pars0$pcol[1])+search_rng)
-prior <- createPrior(density = density_fun_USE, sampler = sampler_fun_USE,
-                     lower = c(pars0$obs, pars0$proc, pars0$pcol[1])-search_rng,
-                     upper = c(pars0$obs, pars0$proc, pars0$pcol[1])+search_rng)
+p0<-list(c(-5, 0), c(-5, 0))
+minvUSE<-unlist(lapply(p0, function(x) x[1]))
+maxvUSE<-unlist(lapply(p0, function(x) x[2]))
 
+p0_edm<-list(c(-5, 0), c(-5, 0), c(-5, 2))
+minvUSE_edm<-unlist(lapply(p0_edm, function(x) x[1]))
+maxvUSE_edm<-unlist(lapply(p0_edm, function(x) x[2]))
+
+density_fun_USE<-function(param) density_fun0(param = param, minv = minvUSE, maxv=maxvUSE)
+sampler_fun_USE<-function(x) sampler_fun0(n = 1, minv = minvUSE, maxv=maxvUSE)
+prior_USE <- createPrior(density = density_fun_USE, sampler = sampler_fun_USE,
+                     lower = minvUSE, upper = maxvUSE)
+
+density_fun_USE_edm<-function(param) density_fun0(param = param, minv = minvUSE_edm, maxv=maxvUSE_edm)
+sampler_fun_USE_edm<-function(x) sampler_fun0(n = 1, minv = minvUSE_edm, maxv=maxvUSE_edm)
+prior_edm <- createPrior(density = density_fun_USE_edm, sampler = sampler_fun_USE_edm,
+                         lower = minvUSE_edm, upper = maxvUSE_edm)
 y<-0
 while(sum(y>0)<=(length(y)/20)) { # want at least 5% nonzero values
   pars_sim<-parseparam0(unname(sampler_fun_USE()))
@@ -58,29 +63,33 @@ while(sum(y>0)<=(length(y)/20)) { # want at least 5% nonzero values
 }
 
 ## Run filter
-p0<-unname(unlist(pars0)[1:3])
-ptrue<-unname(unlist(pars_sim)[1:3])
+ptrue<-unname(unlist(pars_sim)[1:2])
 
 #set number of iterations
-niter<-6e3
+niter<-5000
 N<-2e3
-neff_use<-10
+Euse<-2
 
 #set up likelihoods
-likelihood_detfun0<-function(x) likelihood0(param=x, y=y, parseparam = parseparam0, N = N, neff = neff_use)
-bayesianSetup_detfun0 <- createBayesianSetup(likelihood = likelihood_detfun0, prior = prior)
+likelihood_detfun0<-function(x) likelihood0(param=x, y=y, parseparam = parseparam0, N = N)
+bayesianSetup_detfun0 <- createBayesianSetup(likelihood = likelihood_detfun0, prior = prior_USE)
 
-likelihood_EDM<-function(x) likelihood0(param = x, y=y, parseparam = parseparam0,
-                                        detfun = EDMfun0, edmdat = list(E=2), N = N, neff = neff_use)
-bayesianSetup_EDM <- createBayesianSetup(likelihood = likelihood_EDM, prior = prior)
+likelihood_EDM<-function(x) {
+  xuse<-x[1:2]
+  tuse_edm<-exp(x[3])
+  likelihood0(param = xuse, y=y, parseparam = parseparam0,
+              detfun = EDMfun0, edmdat = list(E=Euse, theta=tuse_edm), N = N)
+}
+
+bayesianSetup_EDM <- createBayesianSetup(likelihood = likelihood_EDM, prior = prior_edm)
 
 #run MCMC chains
 out_detfun0 <- runMCMC(bayesianSetup = bayesianSetup_detfun0, settings = list(iterations=niter, consoleUpdates=100))
 out_EDM <- runMCMC(bayesianSetup = bayesianSetup_EDM, settings = list(iterations=niter, consoleUpdates=100))
 
 ## extract parameters
-smp_detfun0<-getSample(out_detfun0, start = 500)
-smp_EDM<-getSample(out_EDM, start=500)
+smp_detfun0<-getSample(out_detfun0, start = 1000)
+smp_EDM<-getSample(out_EDM, start=1000)
 
 parsest_det<-cbind(colMeans(smp_detfun0), apply(smp_detfun0, 2, sd))
 parsest_edm<-cbind(colMeans(smp_EDM), apply(smp_EDM, 2, sd))
@@ -89,44 +98,27 @@ parsest_edm<-cbind(colMeans(smp_EDM), apply(smp_EDM, 2, sd))
 filterout_det<-particleFilterLL(y, pars=parseparam0(parsest_det[,1]), detfun = detfun0,
                                 dotraceback = TRUE)
 #based on EDM
-filterout_edm<-particleFilterLL(y, pars=parseparam0(parsest_edm[,1]), detfun = EDMfun0, edmdat = list(E=2),
+filterout_edm<-particleFilterLL(y, pars=parseparam0(parsest_edm[1:2,1]), detfun = EDMfun0, edmdat = list(E=2, theta=exp(parsest_edm[3,1])),
                                 dotraceback = TRUE)
 #based on true values
 filterout_true<-particleFilterLL(y, pars=parseparam0(ptrue), detfun = detfun0,
                                 dotraceback = TRUE)
 #based on EDM, with correct values
-filterout_edm_true<-particleFilterLL(y, pars=parseparam0(ptrue), detfun = EDMfun0, edmdat = list(E=2),
+sout<-s_map(y, E=2,silent = TRUE)
+filterout_edm_true<-particleFilterLL(y, pars=parseparam0(ptrue), detfun = EDMfun0, edmdat = list(E=2, theta=sout$theta[which.max(sout$rho)]),
                                 dotraceback = TRUE)
 
 
 ## save outputs
-#from simulated time series
-cm_true<-getcm(datout$true)
-cm_obs<-getcm(datout$obs)
-
-#from extended time series
-datout_long<-makedynamics_general(n = 2e4, n0 = exp(rnorm(1,0,0.1)), pdet=pars_sim$det,
-                                  proc = pars_sim$proc, obs = pars_sim$obs, pcol = pars_sim$pcol,
-                                  detfun = detfun0, procfun = procfun0, obsfun=obsfun0, colfun=colfun0)
-cm_long<-getcm(datout_long$true)
-
-#demographics
-demdat<-list(cm_true=unlist(cm_true), cm_obs=unlist(cm_obs), cm_long=unlist(cm_long),
-             demdat_det=unlist(filterout_det$dem[c("mucol", "mumor")]), demdat_edm=unlist(filterout_edm$dem[c("mucol", "mumor")]), demdat_true=unlist(filterout_true$dem[c("mucol", "mumor")]), demdat_edm_true=unlist(filterout_edm_true$dem[c("mucol", "mumor")]))
-
-#paramters
-pars_sim_realized<-log(c(sd(datout$obs-datout$true), sd(datout$noproc-datout$true)))
-pars_sim_realized_long<-log(c(sd(datout_long$obs-datout_long$true), sd(datout_long$noproc-datout_long$true)))
-
-parslst<-list(pars0=pars0, pars_sim=pars_sim, p0=p0, ptrue=ptrue,
-              pars_sim_realized=pars_sim_realized, pars_sim_realized_long=pars_sim_realized_long,
+parslst<-list(ptrue=ptrue,
               parsest_det=parsest_det, parsest_edm=parsest_edm)
 
 #fits
 cordat<-list(obs=cor(datout$true, datout$obs),
-             det=cor(datout$true, filterout_det$rN),
-             edm=cor(datout$true, filterout_edm$rN),
-             true=cor(datout$true, filterout_true$rN))
+             det=cor(datout$true, filterout_det$Nest),
+             edm=cor(datout$true, filterout_edm$Nest),
+             true=cor(datout$true, filterout_true$Nest),
+             edm_true=cor(datout$true, filterout_edm_true$Nest))
 
 #filter output
 filterdat<-list(filterout_det=filterout_det, filterout_edm=filterout_edm, filterout_true=filterout_true, filterout_edm_true=filterout_edm_true)
@@ -135,9 +127,9 @@ filterdat<-list(filterout_det=filterout_det, filterout_edm=filterout_edm, filter
 optdat<-list(optout_det=out_detfun0, optout_edm=out_EDM)
 
 #simulation outputs
-simdat<-list(datout=datout, datout_long=datout_long)
+simdat<-list(datout=datout)
 
-save(list = c("simdat", "parslst", "optdat", "filterdat", "demdat"), file = paste("datout/mcmcout_", commArgin, "_full.rda", sep=""), version=2)
+save(list = c("simdat", "parslst", "optdat", "filterdat"), file = paste("datout/mcmcout_", commArgin, "_full.rda", sep=""), version=2)
 
 
 
