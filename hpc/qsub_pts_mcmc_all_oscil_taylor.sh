85eb3fbd2e78d822bfab8ada9b08e63e92109b7d
atclark <adam.tclark@gmail.com>
2020-03-05 15:50:03 +0100

    fixed loading

--- hpc/qsub_pts_mcmc_all_oscil_taylor.sh
+++ hpc/qsub_pts_mcmc_all_oscil_taylor.sh
@@ -10,7 +10,10 @@
 #$ -t 1-1000
 
 date
-module load R
+module load GCC/7.3.0-2.30
+module load OpenMPI/3.1.1
+module load R/3.5.1
+
 cd /home/clarka/pts_r_package/hpc/
 ./run_ptstab_mcmc_grad_allvar_oscil_taylor.R $SGE_TASK_ID
 date
